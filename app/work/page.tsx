721dae78fa2489863c66b4e84ce4fab65de745ca
Sarmad Gujjar <sarmad2004ali2004@gmail.com>
2024-09-05 12:57:07 +0500

    add

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -14,12 +14,12 @@ const Page = () => {
           <Image className='w-full h-[550px]' alt='img' width={1000} height={900} src={bg2} />
         </main>
        
-        <main className='absolute flex flex-col gap-3 text-white top-0 left-0 pt-72 pl-56'>
+        <main className='absolute flex flex-col gap-3 text-white top-0 left-0 pt-72 px-10 md:pl-56'>
           <div className='flex gap-5'>
             <span className='flex'>HOME   <span className='text-2xl text-red-600 font-bold flex'>< MdNavigateNext/></span></span>
             <span className='flex'>WORK   <span className='text-2xl text-red-600 font-bold flex'>< MdNavigateNext/></span></span>
           </div>
-          <div className='font-bold text-4xl'>WORK</div>
+          <div className='font-bold text-3xl md:text-4xl'>WORK</div>
         </main>
       </div>
         <Work />
